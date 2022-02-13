2eacd89f194f3494f226eaffccdaf7d56662bb60
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-02-12 23:33:29 -0500

    Added Binaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
-import React from 'react'
-import logo from './logo.svg'
-import tauriCircles from './tauri.svg'
-import tauriWord from './wordmark.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Command } from "@tauri-apps/api/shell";
+import { DefaultButton, PrimaryButton } from "@fluentui/react";
 
 function App() {
+  async function execute() {
+    const command = Command.sidecar("external/binaries/raffle-editor");
+    const output = await command.execute();
+    console.log(output);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="inline-logo">
-          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
-          <img src={tauriWord} className="App-logo smaller" alt="logo" />
-        </div>
-        <a
-          className="App-link"
-          href="https://tauri.studio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tauri
-        </a>
-        <img src={logo} className="App-logo rotate" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <PrimaryButton onClick={execute}>Hola mi panita</PrimaryButton>
+      <DefaultButton>Default</DefaultButton>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
